b2ce1f3eb4951fc80c39db9f6717289fa946b2ec
Francesco Cuscino <hw6335@engie.com>
2022-09-23 12:05:12 +0200

    import

--- src/main/java/com/sella/api/ApiApplication.java
+++ src/main/java/com/sella/api/ApiApplication.java
@@ -1,7 +1,6 @@
 package com.sella.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
